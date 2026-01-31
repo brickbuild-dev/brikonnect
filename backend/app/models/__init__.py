6f9c69eb17f5b00720d65cec30de3cc9c9b3e59f
brickbuild-dev <brickbuildstore@gmail.com>
2026-01-31 12:56:31 +0000

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.tenant import Tenant
-from app.models.user import User
-from app.models.session import Session
-from app.models.inventory import InventoryLot
-from app.models.order import Order, OrderLine
